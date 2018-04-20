9c1c5e46b2c17963fb759ca7668ea63caf3c634b
shenjx <shen0206>
2018-04-20 17:43:00 +0800

    JDK动态代理示例

---
+++ mydesignmode/src/main/java/cn/com/mydesinmode/proxy/jdkproxy/JdkProxyObject2.java
@@ -0,0 +1,43 @@
+package cn.com.mydesinmode.proxy.jdkproxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * Author:   shenjx
+ * Date:     2018/4/20 10:28
+ * Description:代理类
+ */
+public class JdkProxyObject2 implements InvocationHandler {
+
+    private boolean filter(Object proxy,Method method) {
+        Method[] methods=proxy.getClass().getMethods();
+        for (Method m:methods){
+            if(method.getName().equals(m.getName())){
+              return true;
+            }
+        }
+        return false;
+    }
+
+    private Object targetObject;
+    public JdkProxyObject2(Object targetObject){
+        this.targetObject=targetObject;
+    }
+
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        if (filter(proxy,method)) {
+            System.out.println("执行前22");
+
+            Object object = method.invoke(targetObject, args);
+
+            System.out.println("执行后22");
+            return object;
+        } else {
+            return method.invoke(proxy, args);
+        }
+    }
+}
---
+++ mydesignmode/src/main/java/cn/com/mydesinmode/proxy/jdkproxy/MyTestClient.java
@@ -0,0 +1,45 @@
+package cn.com.mydesinmode.proxy.jdkproxy;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * Author:   shenjx
+ * Date:     2018/4/20 11:45
+ * Description:
+ */
+public class MyTestClient {
+
+
+    public static void main(String[] args) {
+        main2();
+    }
+
+    //单层动态代理
+    public static void main1() {
+        try {
+            JdkProxyObject jdkProxyObject = new JdkProxyObject(new TargetObject());
+            ITargetObject iTargetObject = (ITargetObject) Proxy.newProxyInstance(TargetObject.class.getClassLoader(), TargetObject.class.getInterfaces(), jdkProxyObject);
+            iTargetObject.hello();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //多层动态代理
+    public static void main2() {
+        try {
+            JdkProxyObject jdkProxyObject = new JdkProxyObject(new TargetObject());
+            ITargetObject iTargetObject = (ITargetObject) Proxy.newProxyInstance(TargetObject.class.getClassLoader(), TargetObject.class.getInterfaces(), jdkProxyObject);
+
+            JdkProxyObject2 jdkProxyObject2 = new JdkProxyObject2(iTargetObject);
+            iTargetObject = (ITargetObject) Proxy.newProxyInstance(TargetObject.class.getClassLoader(), TargetObject.class.getInterfaces(), jdkProxyObject2);
+
+            iTargetObject.hello();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}

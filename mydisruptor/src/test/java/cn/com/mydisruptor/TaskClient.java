abc76437bde7b1483189d8b69780b1676ebf9ddd
shenjx <shen0206>
2018-04-23 16:58:33 +0800

    添加disruptor工程

---
+++ mydisruptor/src/test/java/cn/com/mydisruptor/TaskClient.java
@@ -0,0 +1,18 @@
+package cn.com.mydisruptor;
+
+import com.lmax.disruptor.dsl.Disruptor;
+
+/**
+ * Copyright (C), 2017-2018, 武汉市胜意科技发展有限公司
+ * Author:   shenjx
+ * Date:     2018/4/23 16:24
+ * Description:
+ */
+public class TaskClient {
+
+
+    public static void main(String[] args) {
+        //Disruptor disruptor=new Disruptor();
+
+    }
+}

03627dad008de14a3e349d19fc2bedff0f4479a8
shenjianxin <shen0206>
2018-04-28 12:21:30 +0800

    线程wait,join等方法使用

--- jdkdemo/src/main/java/cn/com/jdkdemo/mythread/MyJoinTest.java
+++ jdkdemo/src/main/java/cn/com/jdkdemo/mythread/MyJoinTest.java
@@ -6,6 +6,42 @@ import java.util.Date;
 
 public class MyJoinTest {
 
+    public String getTime() {
+        return new SimpleDateFormat("mm:ss:ms").format(new Date());
+    }
+
+    public static void main(String[] args) {
+        MyJoinTest myJoinTest = new MyJoinTest();
+        Thread t1 = myJoinTest.getThreadA();
+        t1.start();
+
+        System.out.println(myJoinTest.getTime() + "子线程A单独执行中,主线程也在继续执行...");
+        System.out.println(myJoinTest.getTime() + "子线程A被join，使主线程阻塞，直到子线程A执行完成...");
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(myJoinTest.getTime() + "主线程执行完成");
+
+
+    }
+
+
+    public Thread getThreadA() {
+        return new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getTime() + "线程A开始执行");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(getTime() + "线程A执行完成");
+            }
+        };
+    }
 
 
 }

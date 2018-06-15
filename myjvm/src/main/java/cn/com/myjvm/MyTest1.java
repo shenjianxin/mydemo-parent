5239436b08e36764d62596a4bdb8ff578ea21574
shenjx <646882597@qq.com>
2018-06-15 17:00:32 +0800

    注释完善

--- myjvm/src/main/java/cn/com/myjvm/MyTest1.java
+++ myjvm/src/main/java/cn/com/myjvm/MyTest1.java
@@ -4,19 +4,33 @@ package cn.com.myjvm;
  * 当要实例化一个类时，JVM会首先加载该类，并且在加载过程中检查这个类是否有静态属性以及静态代码块，如果有，就按顺序分配内存并初始化他们，并且只在类加载的过程中初始化一次。
  * <p>
  * 对于构造代码块，以及普通属性，是在类实例化时进行的，并且每次实例化都会调用，并且构造代码块先于构造方法执行。
+ *
+ *
+ * 会按照如下顺序加载，1、父类中的静态块、静态方法；2、子类中的静态块、静态方法；3、父类的构造块；4、父类的构造方法；5、子类的构造块；6、子类的构造方法。
  */
 public class MyTest1 {
 
+    static {
+        System.out.println(System.currentTimeMillis() + "静态代码块1");
+    }
 
+    static {
+        System.out.println(System.currentTimeMillis() + "静态代码块2");
+    }
     public MyTest1() {
         System.out.println(System.currentTimeMillis() + "构造方法");
     }
     {
-        System.out.println(System.currentTimeMillis() + "构造代码块");
+        System.out.println(System.currentTimeMillis() + "构造代码块1");
     }
-    private String val = System.currentTimeMillis() + "普通属性";
+    {
+        System.out.println(System.currentTimeMillis() + "构造代码块2");
+    }
+    private String val1 = System.currentTimeMillis() + "普通属性1";
+    private String val2 = System.currentTimeMillis() + "普通属性2";
 
-    private static String staticVal = System.currentTimeMillis() + "静态属性";
+    private static String staticVal1 = System.currentTimeMillis() + "静态属性1";
+    private static String staticVal2 = System.currentTimeMillis() + "静态属性2";
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("**************");
@@ -26,9 +40,9 @@ public class MyTest1 {
         System.out.println("**************");
 
 
-        System.out.println(MyTest1.staticVal);
-        System.out.println(myTest1.val);
-        System.out.println(myTest2.val);
+        System.out.println(MyTest1.staticVal1);
+        System.out.println(MyTest1.staticVal2);
+
 
     }
 

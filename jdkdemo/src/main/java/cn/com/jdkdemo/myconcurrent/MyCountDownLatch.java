9516d9878e3dd58c904c8904ab3e60608f440dda
shenjx <646882597@qq.com>
2018-07-28 16:57:09 +0800

    使用CountDownLatch模拟高并发测试

--- jdkdemo/src/main/java/cn/com/jdkdemo/myconcurrent/MyCountDownLatch.java
+++ jdkdemo/src/main/java/cn/com/jdkdemo/myconcurrent/MyCountDownLatch.java
@@ -14,46 +14,39 @@ import java.util.concurrent.TimeUnit;
  */
 public class MyCountDownLatch {
 
-    public static void main(String[] args) throws InterruptedException {
-        //在线程可以通过await（）之前必须调用countDown（）的次数
-        //具有计数1的 CountdownLatch 可以用作”启动大门”，来立即启动一组线程；
+    public static void main(String[] args) {
+        int concurrentNum = 100;//并发数量
+        final CountDownLatch concurrentCountDownLatch = new CountDownLatch(concurrentNum);
         final CountDownLatch begin = new CountDownLatch(1);  //为0时开始执行
-        final ExecutorService exec = Executors.newFixedThreadPool(100);
-        for (int i = 0; i < 100; i++) {
-            final int NO = i + 1;
-            Runnable runnable = new Runnable() {
+        final ExecutorService exec = Executors.newFixedThreadPool(concurrentNum);
+        for (int i = 0; i < concurrentNum; i++) {
+            exec.execute(new Runnable() {
+                @Override
                 public void run() {
                     try {
-                        //如果当前计数为零，则此方法立即返回。
-                        //如果当前计数大于零，则当前线程将被禁用以进行线程调度，并处于休眠状态，直至发生两件事情之一：
-                        //或调用countDown（）方法，计数达到零;
-                        //或一些其他线程中断当前线程。
-
-                        //等待直到 CountDownLatch减到1
                         begin.await();
-                        SortedMap<Object, Object> sortedMap = new TreeMap<>();
-                        sortedMap.put("appid", "5412916052207510000052159");
-                        sortedMap.put("mch_id", "dhfkjadh");
-                        sortedMap.put("nonce_str", "fasjkldfh");
-                        //String sign = SignatureUtil.getSignature(sortedMap, "fhldhfkajhdfkjajsdh");
-                        //System.out.println("sign" + String.valueOf(NO) + ": " + sign);
+                        try {
+                            //todo 需要并发执行的任务
+                            System.out.println(1111);
+                        } finally {
+                            concurrentCountDownLatch.countDown();
+                        }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-            };
-            exec.submit(runnable);
+            });
         }
         System.out.println("开始执行");
-        //减少锁存器的计数，如果计数达到零，释放所有等待的线程。
-        // begin减一，开始并发执行
         begin.countDown();
-        //此方法不等待先前提交的任务完成执行
-        //exec.shutdown();
-        //为了保证先前提交的任务完成执行 使用此方法
-        exec.awaitTermination(4000, TimeUnit.MILLISECONDS);
-
-
+        try {
+            concurrentCountDownLatch.await();
+        } catch (
+                InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("执行结束");
+        exec.shutdown();
     }
 
 

cbe103fac352d74197720a324345c18c6ce0b921
K37 <646882597@qq.com>
2020-11-19 18:19:24 +0800

    线程join的使用场景

--- jdkdemo/src/main/java/cn/com/jdkdemo/mythread/JoinDemo.java
+++ jdkdemo/src/main/java/cn/com/jdkdemo/mythread/JoinDemo.java
@@ -3,6 +3,7 @@ package cn.com.jdkdemo.mythread;
 //Java中如何让多线程按照自己指定的顺序执行？
 public class JoinDemo extends Thread {
     int i;
+
     Thread previousThread; //上一个线程
 
     public JoinDemo(Thread previousThread, int i) {
@@ -14,6 +15,7 @@ public class JoinDemo extends Thread {
     public void run() {
         try {
             //调用上一个线程的join方法
+            //thread.join的含义是当前线程需要等待previousThread线程终止之后才从thread.join返回。简单来说，就是线程没有执行完之前，会一直阻塞在join方法处。
             previousThread.join();
         } catch (InterruptedException e) {
             e.printStackTrace();

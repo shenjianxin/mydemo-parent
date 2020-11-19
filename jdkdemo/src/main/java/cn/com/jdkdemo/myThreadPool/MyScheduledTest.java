58f1785dc7c2e6b03337e3d1e3549c36cb68a66b
K37 <646882597@qq.com>
2020-11-19 18:51:35 +0800

    调度线程

--- jdkdemo/src/main/java/cn/com/jdkdemo/myThreadPool/MyScheduledTest.java
+++ jdkdemo/src/main/java/cn/com/jdkdemo/myThreadPool/MyScheduledTest.java
@@ -18,35 +18,33 @@ public class MyScheduledTest {
         ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(5);
 
 
-//        System.out.println("固定速率开始：" + new Date());
-//        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+        System.out.println("固定速率开始：" + new Date());
+        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("固定速率" + new Date());
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 2, 3, TimeUnit.SECONDS);//固定速率
+
+
+        //上一次执行完成后，等待延时时间才开始执行
+//        System.out.println("固定延时开始：" + new Date());
+//        scheduledExecutorService.scheduleWithFixedDelay(new Runnable() {
 //            @Override
 //            public void run() {
-//
-//                System.out.println(new Date());
+//                System.out.println("固定延时" + new Date());
 //                try {
 //                    Thread.sleep(4000);
 //                } catch (InterruptedException e) {
 //                    e.printStackTrace();
 //                }
 //            }
-//        }, 5, 2, TimeUnit.SECONDS);//固定速率
-
-
-
-        System.out.println("固定延时开始：" + new Date());
-        scheduledExecutorService.scheduleWithFixedDelay(new Runnable() {
-            @Override
-            public void run() {
-
-                System.out.println(new Date());
-//                try {
-//                    Thread.sleep(4000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-            }
-        }, 2, 5, TimeUnit.SECONDS);//固定速率
+//        }, 2, 3, TimeUnit.SECONDS);
 
     }
 }
